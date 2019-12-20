35423f1e6321dc17481d95684a0de5695b5dbb41
ErickMoxley <exilerzero@yahoo.com>
2019-12-20 18:48:50 -0500

    update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Projects from "./Projects.js";
 import Articles from "./Articles.js";
 import About from "./About.js";
+import Home from "./Home";
 import "./App.css";
 import SideNav, { NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStroopwafel } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faStroopwafel);
 
 function App() {
   return (
@@ -24,44 +30,27 @@ function App() {
               <SideNav.Toggle />
               <SideNav.Nav defaultSelected="/">
                 <NavItem eventKey="">
-                  <NavIcon>
-                    <i
-                      className="fa fa-fw fa-home"
-                      style={{ fontSize: "1.75em" }}
-                    />
-                  </NavIcon>
+                  <NavIcon></NavIcon>
                   <NavText>Home</NavText>
                 </NavItem>
                 <NavItem eventKey="about">
                   <NavIcon>
-                    <i
-                      className="fa fa-fw fa-device"
-                      style={{ fontSize: "1.75em" }}
-                    />
+                    <FontAwesomeIcon icon="stroopwafel" size="2x" />
                   </NavIcon>
                   <NavText>About</NavText>
                 </NavItem>
                 <NavItem eventKey="projects">
-                  <NavIcon>
-                    <i
-                      className="fa fa-fw fa-device"
-                      style={{ fontSize: "1.75em" }}
-                    />
-                  </NavIcon>
+                  <NavIcon></NavIcon>
                   <NavText>Projects</NavText>
                 </NavItem>
                 <NavItem eventKey="articles">
-                  <NavIcon>
-                    <i
-                      className="fa fa-fw fa-device"
-                      style={{ fontSize: "1.75em" }}
-                    />
-                  </NavIcon>
+                  <NavIcon></NavIcon>
                   <NavText>Articles</NavText>
                 </NavItem>
               </SideNav.Nav>
             </SideNav>
             <main className="content">
+              <Route exact path="/" component={Home} />
               <Route path="/about" component={About} />
               <Route path="/articles" component={Articles} />
               <Route path="/projects" component={Projects} />
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home(props) {
+  return (
+    <div>
+      <h1>HOMEPAGE Component</h1>
+      <p>HOME testing testing</p>
+    </div>
+  );
+}
+
+export default Home;
