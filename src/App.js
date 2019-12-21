bad8b3a8bf6eb5aae7dd1f1f55ebdeef595baa51
ErickMoxley <exilerzero@yahoo.com>
2019-12-20 21:49:47 -0500

    update

--- src/App.js
+++ src/App.js
@@ -5,59 +5,16 @@ import Articles from "./Articles.js";
 import About from "./About.js";
 import Home from "./Home";
 import "./App.css";
-import SideNav, { NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
-import "@trendmicro/react-sidenav/dist/react-sidenav.css";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStroopwafel } from "@fortawesome/free-solid-svg-icons";
-
-library.add(faStroopwafel);
 
 function App() {
   return (
     <BrowserRouter>
-      <Route
-        render={({ location, history }) => (
-          <React.Fragment>
-            <SideNav
-              onSelect={selected => {
-                const to = "/" + selected;
-                if (location.pathname !== to) {
-                  history.push(to);
-                }
-              }}
-            >
-              <SideNav.Toggle />
-              <SideNav.Nav defaultSelected="/">
-                <NavItem eventKey="">
-                  <NavIcon></NavIcon>
-                  <NavText>Home</NavText>
-                </NavItem>
-                <NavItem eventKey="about">
-                  <NavIcon>
-                    <FontAwesomeIcon icon="stroopwafel" size="2x" />
-                  </NavIcon>
-                  <NavText>About</NavText>
-                </NavItem>
-                <NavItem eventKey="projects">
-                  <NavIcon></NavIcon>
-                  <NavText>Projects</NavText>
-                </NavItem>
-                <NavItem eventKey="articles">
-                  <NavIcon></NavIcon>
-                  <NavText>Articles</NavText>
-                </NavItem>
-              </SideNav.Nav>
-            </SideNav>
-            <main className="content">
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/articles" component={Articles} />
-              <Route path="/projects" component={Projects} />
-            </main>
-          </React.Fragment>
-        )}
-      />
+      <main className="content">
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/articles" component={Articles} />
+        <Route path="/projects" component={Projects} />
+      </main>
     </BrowserRouter>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,72 @@ import React from "react";
 function Home(props) {
   return (
     <div>
-      <h1>HOMEPAGE Component</h1>
-      <p>HOME testing testing</p>
+      <container id="main">
+        <h1>HOMEPAGE Component</h1>
+        <p>HOME testing testing</p>
+      </container>
+
+      <container>
+        <h1>HOMEPAGE Component</h1>
+        <p>HOME testing testing</p>
+      </container>
+
+      <container>
+        <h1>HOMEPAGE Component</h1>
+        <p>
+          Anim non fugiat sunt cupidatat officia excepteur. In ipsum dolore
+          occaecat nostrud tempor dolor aliqua ipsum. Ut incididunt culpa sunt
+          enim ex consequat sunt velit aliquip aute labore tempor do aliquip.
+          Culpa dolor non ullamco sit. Ea voluptate nulla sint culpa amet
+          adipisicing sunt aliqua ut irure sunt esse.
+        </p>
+      </container>
+      <container>
+        <p>
+          Do tempor consectetur sint culpa cupidatat laborum consequat eu. Dolor
+          elit dolore deserunt nisi deserunt fugiat duis dolore dolore cillum
+          mollit amet deserunt est. Veniam aliqua tempor incididunt in sint et
+          sunt amet enim et. Pariatur labore labore amet fugiat. Laborum sunt
+          anim exercitation nisi esse cupidatat fugiat eu ut. Eiusmod mollit
+          mollit cupidatat mollit consectetur in in consequat excepteur nulla
+          amet nostrud labore anim. Fugiat ut fugiat qui duis dolor deserunt ea.
+          Exercitation aute elit exercitation aliquip magna laboris tempor
+          reprehenderit aliquip eiusmod. Voluptate ullamco pariatur ex ut
+          excepteur ut nulla sint aute amet voluptate nulla labore amet. Id aute
+          ullamco sint non aliqua eiusmod ullamco aliquip. Veniam officia aliqua
+          reprehenderit ad ad ut fugiat ullamco duis cillum mollit. Esse nostrud
+          occaecat elit est occaecat enim amet dolor ad excepteur est ipsum in.
+          Mollit deserunt veniam laboris minim et voluptate. Voluptate ex
+          excepteur dolor sit deserunt non non nulla officia dolor cillum velit
+          quis eu. Culpa mollit consectetur eu proident officia commodo
+          adipisicing sunt quis ut. Velit ipsum occaecat ullamco reprehenderit
+          sit enim. Incididunt dolor culpa commodo duis laborum qui deserunt
+          mollit consequat nisi. Eu incididunt et ex eiusmod dolor voluptate
+          aliqua. Elit tempor dolore quis dolore aute fugiat irure. Officia
+          voluptate magna deserunt officia cillum irure dolore et consequat. Et
+          eiusmod reprehenderit irure cillum reprehenderit tempor ea culpa
+          tempor reprehenderit nostrud laboris. Esse nisi aliqua sunt dolore in.
+          Id culpa quis esse dolor ut pariatur pariatur ea reprehenderit.
+        </p>
+
+        <p>
+          Eiusmod ad deserunt anim sunt sint velit minim officia est aliqua ad
+          mollit ut eu. Id elit incididunt incididunt tempor et consequat dolore
+          non ea. Eiusmod elit voluptate sit esse sint sunt consequat. Veniam eu
+          est reprehenderit ipsum et esse duis anim aliquip elit elit anim. Amet
+          voluptate irure consectetur minim eiusmod proident et ad voluptate
+          irure ut. Pariatur occaecat fugiat aute irure voluptate Lorem aliqua
+          in in. Id anim ad non cillum duis voluptate nisi nostrud incididunt id
+          ipsum excepteur voluptate qui. Aliqua laboris proident fugiat ad sunt
+          et laboris occaecat laboris qui culpa non. Id consequat exercitation
+          duis ad anim. Deserunt anim esse ad ad officia exercitation esse.
+          Nulla est nostrud aute qui.
+        </p>
+      </container>
+
+      <div>test test </div>
+
+      <div>test test test test</div>
     </div>
   );
 }
