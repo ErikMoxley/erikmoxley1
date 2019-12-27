deca9293ebe0c6cbdef78b543c4836b062d547d6
ErickMoxley <exilerzero@yahoo.com>
2019-12-27 05:07:01 -0500

    update

--- src/Footer.js
+++ src/Footer.js
@@ -1,57 +1,134 @@
 import React from "react";
 import "./style/Footer.css";
 import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHtml5,
+  faCss3,
+  faJs,
+  faBootstrap,
+  faFontAwesome,
+  faReact,
+  faInstagram,
+  faTwitch,
+  faGithub,
+  faLinkedin,
+  faDiscord,
+  faFacebook
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <MDBFooter color="black" className="font-small pt-4 mt-4">
-      <MDBContainer fluid className="text-center text-md-left">
+    <MDBFooter color="black" className="footer-bot pt-4 mt-4">
+      <MDBContainer fluid className="footer-container text-center text-md-left">
         <MDBRow>
-          <MDBCol md="3">
+          <MDBCol className="footer-quote" md="3">
+            <h2 className="footer-brand" md="3">
+              Erik Moxley
+            </h2>
+            <p>
+              "In the expression of grief lies recovery from grief itself." -
+              Christopher Priest
+            </p>
+          </MDBCol>
+          <MDBCol className="footer-content1" md="3">
             <h5 className="title">Content</h5>
-            <p>Content will be written here.</p>
-          </MDBCol>
-          <MDBCol md="3">
-            <h5 className="title">Info Content</h5>
             <ul>
-              <li>One</li>
-              <li>Two</li>
-              <li>Three</li>
-              <li>Four</li>
+              <ul>
+                <a href="/">Home</a>
+              </ul>
+              <ul>
+                <a href="#about">About</a>
+              </ul>
+              <ul>
+                <a href="#projects">Projects</a>
+              </ul>
+              <ul>Blog (Coming Soon!)</ul>
             </ul>
           </MDBCol>
-          <MDBCol md="3">
-            <h5 className="title">Social Links</h5>
+          <MDBCol className="footer-social" md="3">
+            <h5 className="title">Social</h5>
             <ul>
               <li className="list-unstyled">
-                <a href="#!">Link 1</a>
+                <a
+                  href="https://github.com/erikmoxley"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faGithub} size="2x" />
+                  Github
+                </a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 2</a>
+                <a
+                  href="https://www.linkedin.com/in/erikmoxley/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Linked
+                  <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                </a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 3</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 4</a>
+                <a
+                  href="https://discordapp.com/invite/58SevRg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faDiscord} size="2x" />
+                  &nbsp; Discord
+                </a>
               </li>
             </ul>
           </MDBCol>
-          <MDBCol md="3">
-            <h5 className="title">Made With Content</h5>
+          <MDBCol className="footer-social" md="3">
+            <h5 className="title">Follow</h5>
             <ul>
-              <li>One</li>
-              <li>Two</li>
-              <li>Three</li>
-              <li>Four</li>
+              <li className="list-unstyled">
+                <a
+                  href="https://www.facebook.com/vincenterikmoxley"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faFacebook} size="2x" />
+                  acebook
+                </a>
+              </li>
+              <li className="list-unstyled">
+                <a
+                  href="https://www.instagram.com/erik.moxley"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Instagram <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+              </li>
+              <li className="list-unstyled">
+                <a
+                  href="https://www.twitch.tv/erickmoxley"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faTwitch} size="2x" />
+                  Twitch.tv
+                </a>
+              </li>
             </ul>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
-      <div className="footer-copyright text-center py-3">
-        <MDBContainer fluid>Content made with listed here.</MDBContainer>
+      <div className="footer-content text-center py-3">
+        <MDBContainer className="made-with" fluid>
+          Content made possible with:<br></br>
+          <FontAwesomeIcon icon={faHtml5} size="6x" />
+          <FontAwesomeIcon icon={faCss3} size="6x" />
+          <FontAwesomeIcon icon={faJs} size="6x" />
+          <FontAwesomeIcon icon={faBootstrap} size="6x" />
+          <FontAwesomeIcon icon={faFontAwesome} size="6x" />
+          <FontAwesomeIcon icon={faReact} size="6x" />
+        </MDBContainer>
       </div>
-      <div className="footer-copyright text-center py-3">
+      <div className="footer-copyright text-left py-3">
         <MDBContainer fluid>
           &copy; {new Date().getFullYear()} Copyright: {"Erik Moxley"}
         </MDBContainer>
