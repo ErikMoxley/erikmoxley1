6f7571b9c15d11d25cf7dd2f2effe4f77eca9a91
ErickMoxley <exilerzero@yahoo.com>
2019-12-30 05:34:09 -0500

    Updates

--- precache-manifest.9b6a60c5bb9237b8783ac1409faed5cb.js
+++ precache-manifest.9b6a60c5bb9237b8783ac1409faed5cb.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c63b7dc6e12f79f857386b1d1be06635",
+    "revision": "94904a8e3004dd6c8aac17bbda910fd6",
     "url": "/index.html"
   },
   {
-    "revision": "c407a21d5606758b5194",
+    "revision": "fa424b15b79bc8adb5cb",
     "url": "/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "edc4840be8fe010e9fed",
+    "revision": "6e728928646d57bcb53f",
     "url": "/static/css/main.a7a70c9c.chunk.css"
   },
   {
-    "revision": "c407a21d5606758b5194",
-    "url": "/static/js/2.d7f681a4.chunk.js"
+    "revision": "fa424b15b79bc8adb5cb",
+    "url": "/static/js/2.98c139f6.chunk.js"
   },
   {
     "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
-    "url": "/static/js/2.d7f681a4.chunk.js.LICENSE"
+    "url": "/static/js/2.98c139f6.chunk.js.LICENSE"
   },
   {
-    "revision": "edc4840be8fe010e9fed",
-    "url": "/static/js/main.8655a9d1.chunk.js"
+    "revision": "6e728928646d57bcb53f",
+    "url": "/static/js/main.e85e9bc1.chunk.js"
   },
   {
     "revision": "440b89f768632884dc04",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b5aaa8185e8e735b312086f55ec2d20.js"
+  "/precache-manifest.9b6a60c5bb9237b8783ac1409faed5cb.js"
 );
 
 self.addEventListener('message', (event) => {
