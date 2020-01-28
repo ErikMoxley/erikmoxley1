67bac60f0b2af1e810999acab18dd3aa358ebe50
ErickMoxley <exilerzero@yahoo.com>
2020-01-28 00:54:07 -0500

    update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <main>
         <Switch>
           <Route exact path="/" component={Home} />
-          {/* <Route exact path="/about" component={About} /> */}
+          {/* <Route exact path="/blog" component={Blog} /> */}
         </Switch>
       </main>
     </BrowserRouter>
